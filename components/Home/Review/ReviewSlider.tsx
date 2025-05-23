e8d2dbca82901846e6c2ae7a215bf39041948f73
simgemermertas06 <simgemermertas@gmail.com>
2025-05-23 18:23:38 +0300

    Remove unused Image import from ReviewSlider

--- components/Home/Review/ReviewSlider.tsx
+++ components/Home/Review/ReviewSlider.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import Image from "next/image";
 import ReviewCard from "./ReviewCard";
 
 const responsive = {
